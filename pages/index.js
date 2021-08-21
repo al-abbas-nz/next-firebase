dd6401ebba5437fc466cb3b346cb160c2083cf33
al-abbas-nz <al.mus930@gmail.com>
2021-08-21 16:47:38 +1200

    add react-hot-toast

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import styles from '../styles/Home.module.css';
 import Loader from '../components/Loader';
+import toast from 'react-hot-toast';
 export default function Home() {
   return (
     <div>
+      <button onClick={() => toast.success('hello toast!')}>
+        Toast me bruh
+      </button>
       <Loader show />
     </div>
   );
