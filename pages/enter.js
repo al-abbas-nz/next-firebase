5dfee7fb1c51f7b22246de395455d849ff7692bb
al-abbas-nz <al.mus930@gmail.com>
2021-08-22 18:03:12 +1200

    connect to firestore and write to docs; username functionality

--- pages/enter.js
+++ pages/enter.js
@@ -1,6 +1,7 @@
-import { auth, googleAuthProvider } from '../lib/firebase';
-import { useContext } from 'react';
+import { auth, firestore, googleAuthProvider } from '../lib/firebase';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { UserContext } from '../lib/context';
+import debounce from 'lodash.debounce';
 
 export default function EnterPage({}) {
   const { user, username } = useContext(UserContext);
@@ -27,13 +28,8 @@ export default function EnterPage({}) {
 // Sign in with Google button
 function SignInButton() {
   const signInWithGoogle = async () => {
-    try {
-      await auth.signInWithPopup(googleAuthProvider);
-    } catch (error) {
-      console.error(error);
-    }
+    await auth.signInWithPopup(googleAuthProvider);
   };
-
   return (
     <button className='btn-google' onClick={signInWithGoogle}>
       <img src={'/google.png'} /> Sign in with Google
@@ -45,4 +41,109 @@ function SignOutButton() {
   return <button onClick={() => auth.signOut()}>Sign Out</button>;
 }
 
-function UsernameForm() {}
+function UsernameForm() {
+  const [formValue, setFormValue] = useState('');
+  const [isValid, setIsValid] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const { user, username } = useContext(UserContext);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    // Create refs for both documents
+    const userDoc = firestore.doc(`users/${user.uid}`);
+    const usernameDoc = firestore.doc(`usernames/${formValue}`);
+
+    // Commit both docs together as a batch write.
+    const batch = firestore.batch();
+
+    batch.set(userDoc, {
+      username: formValue,
+      photoURL: user.photoURL,
+      displayName: user.displayName,
+    });
+
+    batch.set(usernameDoc, { uid: user.uid });
+
+    await batch.commit();
+  };
+
+  const onChange = (e) => {
+    // Force form value typed in form to match correct format
+    const val = e.target.value.toLowerCase();
+    const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+    if (val.length < 3) {
+      setFormValue(val);
+      setLoading(false);
+      setIsValid(false);
+    }
+
+    if (re.test(val)) {
+      setFormValue(val);
+      setLoading(true);
+      setIsValid(false);
+    }
+  };
+
+  useEffect(() => {
+    checkUsername(formValue);
+  }, [formValue]);
+
+  // hit the database for username match after each debounced change
+  // useCallback is requred for debounce to work
+  const checkUsername = useCallback(
+    debounce(async (username) => {
+      if (username.length >= 3) {
+        const ref = firestore.doc(`usernames/${username}`);
+        const { exists } = await ref.get();
+        console.log('Firestore read executed!');
+        setIsValid(!exists);
+        setLoading(false);
+      }
+    }, 500),
+    []
+  );
+
+  return (
+    !username && (
+      <section>
+        <h3>Choose Username</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            name='username'
+            placeholder='username'
+            value={formValue}
+            onChange={onChange}
+          />
+          <UsernameMessage
+            username={formValue}
+            isValid={isValid}
+            loading={loading}
+          />
+          <button type='submit' className='btn-freen' disabled={!isValid}>
+            Choose
+          </button>
+          <h3>Debug State</h3>
+          <div>Username: {formValue}</div>
+          <br />
+          Loading: {loading.toString()}
+          <br />
+          Username Valid: {isValid.toString()}
+        </form>
+      </section>
+    )
+  );
+}
+
+function UsernameMessage({ username, isValid, loading }) {
+  if (loading) {
+    return <p>Checking... </p>;
+  } else if (isValid) {
+    return <p className='text-success'>{username} is available!</p>;
+  } else if (username && !isValid) {
+    return <p className='text-danger'>That username is taken!</p>;
+  } else {
+    return <p></p>;
+  }
+}
