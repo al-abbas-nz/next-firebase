11eb080694f1057c09e65b2a331304ccc9f5b734
al-abbas-nz <al.mus930@gmail.com>
2021-08-28 16:16:40 +1200

    add heart functionality

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -2,6 +2,9 @@ import styles from '../../styles/Post.module.css';
 import { useDocumentData } from 'react-firebase-hooks/firestore';
 import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase';
 import PostContent from '../../components/PostContent';
+import AuthCheck from '../../components/AuthCheck';
+import HeartButton from '../../components/HeartButton';
+import Link from 'next/link';
 
 export async function getStaticProps({ params }) {
   const { username, slug } = params;
@@ -60,6 +63,16 @@ export default function Post(props) {
         <p>
           <strong>{post.heartCount || 0} 🤍</strong>
         </p>
+
+        <AuthCheck
+          fallback={
+            <Link href='/enter'>
+              <button>❤ Sign Up</button>
+            </Link>
+          }
+        >
+          <HeartButton postRef={postRef} />
+        </AuthCheck>
       </aside>
     </main>
   );
